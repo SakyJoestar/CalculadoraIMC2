2564c6143ef0fbaafe2f98f0cf6b6c01234e5d59
Samuel Galindo <itzsaky@outlook.com>
2024-08-29 19:31:55 -0500

    Cambio de variables resultado

--- app/src/main/java/com/example/calculadoraimc2/MainActivity.kt
+++ app/src/main/java/com/example/calculadoraimc2/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.calculadoraimc2
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.calculadoraimc2.databinding.ActivityMainBinding
+import java.util.Locale
 import kotlin.math.pow
 
 class MainActivity : AppCompatActivity() {
@@ -21,8 +22,7 @@ class MainActivity : AppCompatActivity() {
             val estaturaTxt= binding.editTextEstatura.text.toString()
             if(pesoTxt.isEmpty()|| estaturaTxt.isEmpty())
             {
-                binding.textViewResultadoMostrar.text = "Rellene bien los datos"
-            }
+                binding.textViewResultadoMostrar.text = getString(R.string.salidaErrorCampos)            }
             else{
 
                 val peso = pesoTxt.toFloat()
@@ -35,16 +35,14 @@ class MainActivity : AppCompatActivity() {
     private fun calcularIMC(peso: Float, estatura: Float){
         val imc = peso/(estatura.pow(2))
 
-        lateinit var resultado: String
-
-        resultado = when {
+        val resultado = when {
             imc < 18.5 -> "Bajo peso"
             imc in 18.5..24.9 -> "Peso normal"
             imc in 25.0..29.9 -> "Sobrepeso"
             else -> "obesidad"
         }
 
-        val imcRedondeado = String.format("%.2f", imc)
+        val imcRedondeado = String.format(Locale.US,"%.2f", imc)
         val finaltext= "$imcRedondeado\n$resultado"
         binding.textViewResultadoMostrar.text = finaltext
     }
